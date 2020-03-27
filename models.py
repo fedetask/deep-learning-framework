7b18fa5e2549430ead05857e9814c6f014f7abe4
Federico <federico.taschin@studenti.unipd.it>
2020-03-27 18:19:33 +0100

    (Fix commit) Add model class
    Fix commit 07fd4ee6a06bf75ca4b4ee875062da85828bd7d8 added the models.py that
    added the file without the content

---
+++ models.py
@@ -0,0 +1,27 @@
+from computation_graph import Values, Sum
+from layers import Dense
+import numpy as np
+
+
+class Model:
+
+    def __init__(self, inputs, outputs):
+        self.inputs = inputs
+        self.outputs = outputs
+
+    def predict(self, input_values):
+        if isinstance(self.inputs, list):
+            assert len(self.inputs) == len(input_values)
+
+        self.inputs.set_values(input_values)
+        return self.outputs.eval()
+
+    def __call__(self, input_values):
+        return self.predict(input_values)
+
+
+if __name__ == '__main__':
+    x = Values(shape=(None, 1, 5))
+    k = Dense(units=16)(x)
+    model = Model(x, k)
+    res = model(np.ones(shape=(20, 1, 5)))
